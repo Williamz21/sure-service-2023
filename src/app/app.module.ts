f2005f6435a3349ee6055db4106e5781f4e1a081
Williamz21 <u201816224@upc.edu.pe>
2023-05-01 18:43:43 -0500

    Updated imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,20 +9,24 @@ import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
-import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatSelectModule} from '@angular/material/select';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
 
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import { HttpClientModule } from "@angular/common/http";
 import { AuthComponent } from './auth/pages/auth/auth.component';
 import { LoginComponent } from './auth/pages/login/login.component';
 import { RegisterComponent } from './auth/pages/register/register.component';
+import { HomeComponent } from './home/pages/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AuthComponent,
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
@@ -32,7 +36,9 @@ import { RegisterComponent } from './auth/pages/register/register.component';
     MatInputModule,
     MatIconModule,
     MatButtonModule,
-    MatSidenavModule,
+    MatSelectModule,
+    MatRadioModule,
+    MatSnackBarModule,
     FormsModule, ReactiveFormsModule,
     HttpClientModule,
   ],
