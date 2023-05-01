7ccc6d2d6e92600a3152fbabce4f8313eb908a4d
Williamz21 <u201816224@upc.edu.pe>
2023-05-01 18:42:26 -0500

    Created home component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,15 +3,17 @@ import { RouterModule, Routes } from '@angular/router';
 import { AuthComponent } from './auth/pages/auth/auth.component';
 import { LoginComponent } from './auth/pages/login/login.component';
 import { RegisterComponent } from './auth/pages/register/register.component';
+import { HomeComponent } from './home/pages/home/home.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '', pathMatch: 'full' },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
   {
     path: 'auth', component: AuthComponent, children: [
       { path: 'login', component: LoginComponent },
       { path: 'register', component: RegisterComponent }
     ]
-  }
+  },
+  { path: 'home', component: HomeComponent },
 ];
 
 @NgModule({
